077dd5476b5a1c828ed64b3a43820ae05f27b67e
ProjectXero <projectxero@163.com>
2023-07-13 16:13:51 +0800

    release 0.1.0

--- main.ts
+++ main.ts
@@ -1,137 +1,72 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { MarkdownView, Plugin, WorkspaceLeaf } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
+const MarkdownViewScrollOffset = Symbol('MarkdownViewScrollOffset');
+const MarkdownViewScrollGroup = Symbol('MarkdownViewScrollGroup');
 
-interface MyPluginSettings {
-	mySetting: string;
+declare module 'obsidian' {
+	interface WorkspaceLeaf {
+		group: string | null;
+		workspace: Workspace;
+		working: boolean;
+	}
+
+	interface MarkdownView {
+		syncState(this: MarkdownView, sameType: boolean): Promise<boolean>;
+		getScrollOffsetForSync(this: MarkdownView, group: string): number;
+		getSyncViewState(this: MarkdownView): OpenViewState;
+		[MarkdownViewScrollOffset]: number | undefined;
+		[MarkdownViewScrollGroup]: string | undefined;
+	}
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
-
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class MarkdownSyncScrollPlugin extends Plugin {
+	originalSyncState: MarkdownView['syncState'];
 
 	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.originalSyncState = MarkdownView.prototype.syncState;
+        MarkdownView.prototype.getScrollOffsetForSync = function(group) {
+            let scrollOffset = this[MarkdownViewScrollOffset];
+            if (group !== this[MarkdownViewScrollGroup]) {
+                scrollOffset = undefined;
+            }
+            if (scrollOffset === undefined) {
+                const currentScroll = this.currentMode.getScroll();
+                this[MarkdownViewScrollOffset] = currentScroll;
+                this[MarkdownViewScrollGroup] = group;
+				return currentScroll;
+            }
+            return scrollOffset;
+        }
+        MarkdownView.prototype.syncState = async function(sameType) {
+            const leaf = this.leaf as unknown as WorkspaceLeaf;
+            const group = leaf.group;
+            if (!group) return false;
+            const syncViewState = this.getSyncViewState();
+            const currentScroll = this.currentMode.getScroll();
+            const srcScrollOffset = this.getScrollOffsetForSync(group);
+            let success = true;
+            for (const groupLeaf of leaf.workspace.getGroupLeaves(group)) {
+                if (groupLeaf === leaf) continue;
+                const isSameType = groupLeaf.view.getViewType() === this.getViewType();
+                if (!sameType || isSameType) {
+                    if (groupLeaf.working) {
+                        success = false;
+                        continue;
+                    }
+                    const destView = groupLeaf.view;
+                    if (destView instanceof MarkdownView) {
+                        const destScrollOffset = destView.getScrollOffsetForSync(group);
+                        destView.currentMode.applyScroll(currentScroll - srcScrollOffset + destScrollOffset);
+                    } else {
+                        await groupLeaf.openFile(this.file, syncViewState);
+                    }
+                }
+            }
+            return success;
+        }
 	}
 
 	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+		MarkdownView.prototype.syncState = this.originalSyncState;
 	}
 }
